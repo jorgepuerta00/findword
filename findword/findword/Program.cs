32bc44182a6b48a74121edbc1ed13e3d69da973f
Jorge Puerta <jorgepuerta@Jorges-MacBook-Air.local>
2021-08-01 07:13:11 -0500

    first code review

---
+++ findword/findword/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WordFinder
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix = new string[] { "chill", "wind", "snow", "cold" };
+            var wordstream = new string[] { "abcdc", "fgwio", "chill", "pqnsd", "uvdxy" };
+            var result = new WordFinder(matrix).Find(wordstream);
+            Console.WriteLine(string.Join(",", result));
+        }
+    }
+}
---
+++ findword/findword/WordFinder.cs
@@ -0,0 +1,42 @@
+﻿namespace WordFinder
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class WordFinder
+    {
+        private readonly HashSet<string> matrix;
+		
+		public WordFinder(IEnumerable<string> matrix)
+		{
+			this.matrix = new HashSet<string>(matrix);
+		}
+		
+		public IEnumerable<string> Find(IEnumerable<string> wordstream)
+		{
+			var leftRightSearchString = string.Join(string.Empty, wordstream);
+			
+			var characterMatrix = wordstream
+				.Select(row => row.ToCharArray())
+				.ToArray();
+			var topDownSearchStringBuilder = new StringBuilder();
+			for (var i = 0; i < characterMatrix.Length; i++)
+			{
+				for(var j = 0; j < characterMatrix[i].Length; j++)
+				{
+					topDownSearchStringBuilder.Append(characterMatrix[j][i]);
+				}
+			}
+			var topDownSearchString = topDownSearchStringBuilder.ToString();
+			
+			var resultSet = new HashSet<string>();
+			resultSet.UnionWith(matrix.Where(searchTerm => 
+												 leftRightSearchString.Contains(searchTerm)));
+			resultSet.UnionWith(matrix.Where(searchTerm => 
+												 topDownSearchString.Contains(searchTerm)));
+			
+			return resultSet.ToList();
+		}
+    }
+}
